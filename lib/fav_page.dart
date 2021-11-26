10f88d6f0822be89e316cf5e473684832b23ce1f
FlutLab.io <>
2021-11-26 22:33:16 +0000

    Autosave

---
+++ lib/fav_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatefulWidget {
+  @override
+  _InitialPageState createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  String temaGif = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
