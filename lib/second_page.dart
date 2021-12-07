732574acd32dde33bc075f8d2d98b39036a4263b
FlutLab.io <>
2021-12-07 01:17:51 +0000

    Autosave

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -16,18 +16,10 @@ class _SecondPageState extends State<SecondPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SingleChildScrollView(
-        controller: _controller,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              print(widget.gifs[index]);
-              return Image.network(widget.gifs[index]);
-            },
-            itemCount: widget.gifs.length,
-          ),
-        ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image.network(widget.gifs[0]),
       ),
     );
   }
