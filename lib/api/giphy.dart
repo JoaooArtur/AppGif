29c0e90a4eb25194bf3feca2235fbab3e6b218e5
FlutLab.io <>
2021-12-07 00:01:55 +0000

    Autosave

--- lib/api/giphy.dart
+++ lib/api/giphy.dart
@@ -3,8 +3,7 @@ import 'dart:convert' as convert;
 
 class GiphyAPI {
   static Future<List<String>> BuscarGifs(String search, int offset) async {
-    String url = "api.giphy.com";
-    var uri = Uri.https(url, '/v1/gifs/search?api_key=1CYYivEibVP4tvKDcSOzssCRicN4p5Cz&p=${search}teste&limit=10&offset=${offset}&rating=g&lang=pt');
+    var uri = Uri.https('api.giphy.com/v1/gifs/search?api_key=1CYYivEibVP4tvKDcSOzssCRicN4p5Cz&p=${search}&limit=10&offset=${offset}&rating=g&lang=pt', '');
     print(uri);
     var response = await http.get(uri);
     print(response.body);
